657c42e0ac7a709a05808060482bfa267857d623
Dmitriy Fishman <fishman.code@gmail.com>
2021-06-07 10:35:49 +0300

    Update config.js
    Removed Easter eggs

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-// “In a software project team of ten, there are probably three people who produce enough defects to make them net-negative producers.”
 export const Server = {
     endpoint : process.env.REACT_APP_ENDPOINT,
     project: process.env.REACT_APP_PROJECT,
