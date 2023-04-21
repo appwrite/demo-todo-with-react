4bdfcbf6cd0c4dd4e95417c0e6169c001020ad82
Loïc Kalbermatter <52981359+loickal@users.noreply.github.com>
2023-04-21 15:56:02 +0200

    feat: update Branding

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,24 +1,24 @@
-import { appwrite, github, twitter, react } from "../icons";
-import { useHistory } from "react-router-dom";
+import { appwrite, github, twitter, react } from '../icons';
+import { useHistory } from 'react-router-dom';
 
 const Landing = () => {
   const history = useHistory();
 
   const handleClick = () => {
-    history.push("/todos");
+    history.push('/todos');
   };
 
   const links = [
     {
-      href: "http://github.com/appwrite/appwrite",
+      href: 'http://github.com/appwrite/appwrite',
       icon: github(10),
     },
     {
-      href: "https://twitter.com/appwrite_io",
+      href: 'https://twitter.com/appwrite_io',
       icon: twitter(10),
     },
     {
-      href: "http://appwrite.io",
+      href: 'http://appwrite.io',
       icon: appwrite(10),
     },
   ];
@@ -28,24 +28,24 @@ const Landing = () => {
       <section className="container h-screen mx-auto flex">
         <div className="flex flex-col mx-auto justify-center p-6 text-center">
           <p className="my-8 text-xl md:text-2xl lg:text-3xl font-medium">Introducing</p>
-          <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold">toTooooDoooo</h1>
+          <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold">RainyDo</h1>
           <p className="my-8 text-xl md:text-2xl lg:text-3xl font-medium">
-            A Simple To-do App built with {appwrite(8)} Appwrite and {react(8)}{" "}
-            React
+            A Simple To-do App provided by RainySystems
           </p>
           <button
             onClick={handleClick}
-            className="mx-auto mt-4 py-3 lg:py-5 px-10 lg:px-24 text-lg md:text-2xl font-semibold  rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none"
-          >
-            Get Started
+            className="mx-auto mt-4 py-3 lg:py-5 px-10 lg:px-24 text-lg md:text-2xl font-semibold  rounded-lg shadow-md bg-white text-gray-900 border border-gray-900 hover:border-transparent hover:text-white hover:bg-gray-900 focus:outline-none">
+            Jump in!
           </button>
         </div>
       </section>
 
       <section className="absolute bottom-0 right-0 py-3 px-6 mr-8 mb-8 flex">
         {links.map((item, key) => (
-          <div key={key} className="rounded-full mx-4 transition duration-200 ease-in-out transform hover:-translate-y-3 hover:scale-125 hover:shadow-4xl">
-            <a href={item["href"]}>{item["icon"]}</a>
+          <div
+            key={key}
+            className="rounded-full mx-4 transition duration-200 ease-in-out transform hover:-translate-y-3 hover:scale-125 hover:shadow-4xl">
+            <a href={item['href']}>{item['icon']}</a>
           </div>
         ))}
       </section>
