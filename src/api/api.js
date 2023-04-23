9d555bcafca3a1f108405f212c7fd183002db068
Loïc Kalbermatter <52981359+loickal@users.noreply.github.com>
2023-04-23 22:53:45 +0200

    feat: try fixing discord login issue

--- src/api/api.js
+++ src/api/api.js
@@ -51,7 +51,7 @@ let api = {
   },
 
   createOAuth2Session: (provider) => {
-    return api.provider().account.createOAuth2Session(provider, 'https://rainydo.ch');
+    return api.provider().account.createOAuth2Session(provider, 'https://rainydo.ch/login');
   },
 };
 
