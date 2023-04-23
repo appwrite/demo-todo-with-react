37a8e135d8e3230f45f579d82aa135de318fa88d
Loïc Kalbermatter <52981359+loickal@users.noreply.github.com>
2023-04-23 23:20:03 +0200

    feat: add frontendUrl to env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 export const Server = {
-    endpoint : process.env.REACT_APP_ENDPOINT,
-    project: process.env.REACT_APP_PROJECT,
-    collectionID : process.env.REACT_APP_COLLECTION_ID,
-    databaseID : process.env.REACT_APP_DATABASE_ID,
-}
+  endpoint: process.env.REACT_APP_ENDPOINT,
+  project: process.env.REACT_APP_PROJECT,
+  collectionID: process.env.REACT_APP_COLLECTION_ID,
+  databaseID: process.env.REACT_APP_DATABASE_ID,
+  frontendUrl: process.env.REACT_APP_FRONTEND_URL,
+};
